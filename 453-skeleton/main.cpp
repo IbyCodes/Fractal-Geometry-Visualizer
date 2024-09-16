d3906dacc8c7a14cbc6ba49996cd36cb8f907394
Mohammad Ibrahim Khan <ibrahimkhan786007@gmail.com>
2024-09-15 23:31:29 -0600

    Figured out Sierpinksi Triangle fractal
    Need to organize code better

--- 453-skeleton/main.cpp
+++ 453-skeleton/main.cpp
@@ -1,26 +1,22 @@
 // LEARNOPENGL - GOOD SITE TO learn how to draw things properly
-
 // clip space represents the space in which the GPU operates
 // everything within a clip space gets drawn into the screen, everything else doesn't
 // this clip space is a 2x2 cube centered at the origin, middle of cube
 // towards the top is pos y axis, to the right is pos x axis, and out of the screen is pos z axis
 // basically, every vertice you provide for this assignment must be between (-1,-1,-1) and (1,1,1)
-
 // opengl shaders are in glsl (dont need to know about for assignment #1)
+// FOLLOWED PARAMETERS CODE in tutorial to deal with the fact that we have multiple shapes that we gotta draw, not just 1
 
-
-// FOLLOW PARAMETERS CODE in tutorial to deal with the fact that we have multiple shapes that we gotta draw, not just 1 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
-
 #include "Geometry.h"
 #include "GLDebug.h"
 #include "Log.h"
 #include "ShaderProgram.h"
 #include "Shader.h"
 #include "Window.h"
+#include <cstdlib>  // For rand() and RAND_MAX
 
 
 struct Parameters { // struct for parameters for user input example
@@ -33,6 +29,9 @@ struct Parameters { // struct for parameters for user input example
 	bool isDifferent(Parameters p) { // for efficiency reasons, will check to see if any change was made (with the arrows from the user controlling them)
 		return p.t != t || p.u != u;
 	}
+
+	int iterations = 0; // New parameter for Sierpinski iterations
+
 };
 
 // EXAMPLE CALLBACKS
@@ -48,10 +47,12 @@ public:
 			}
 			if (key == GLFW_KEY_UP) { // if we press the up arrow the 't' parameter will increase a little
 				parameters.t += parameters.tStep;
+				parameters.iterations = std::min(parameters.iterations + 1, 10); // Cap at 10 iterations
 			}
 
 			if (key == GLFW_KEY_DOWN) { // if we press the down arrow the 't' parameter will decrease a little
 				parameters.t -= parameters.tStep;
+				parameters.iterations = std::max(parameters.iterations - 1, 0); // Minimum 0 iterations
 			}
 
 			if (key == GLFW_KEY_LEFT) { // if we press the LEFT arrow the 'u' parameter will decrease a little
@@ -87,15 +88,12 @@ public:
 };
 // END EXAMPLES (the above things are just example pieces of code)
 
-
-
 /*
 std::vector<glm::vec3> sqSpiral(int i){
 	std::vector<glm::vec3> ret;
 	float step = 0.2;
 	bool axis = false;
 	bool direction = false;
-
 	glm::vec3 currentPos(0., 0., 0.);
 
 	for (int it = 0; it < i; it++){
@@ -118,7 +116,6 @@ std::vector<glm::vec3> sqSpiral(int i){
 			}
 		}
 	}
-
 	ret.push_back(newpos);
 	currentPos = newpos;
 
@@ -131,11 +128,92 @@ CPU_Geometry generateSin(Parameters p) { // originally from main, but now put in
 	CPU_Geometry cpuGeom;
 	for (float x = -1.0f; x <= 1.0f; x += 0.01f) {
 		cpuGeom.verts.push_back(glm::vec3(x, sin(x*p.t)*p.u, 0.0));
-		cpuGeom.cols.push_back(glm::vec3(cos(x), sin(x), 0.0));
+		cpuGeom.cols.push_back(glm::vec3(0.6, 0.2, 0.0));
 	}
 	return cpuGeom;
 
 }
+
+CPU_Geometry generateSierpinski(int iterations) {
+	CPU_Geometry cpuGeom;
+
+	// Initial triangle vertices (an equilateral triangle)
+	glm::vec3 p1(-0.5f, -0.5f, 0.f);
+	glm::vec3 p2(0.5f, -0.5f, 0.f);
+	glm::vec3 p3(0.f, 0.5f, 0.f);
+
+
+	/*
+	// function to subdivide the sierpinksi triangle fractal (from notes)
+	void divide_triangle(glm::vec3 pointA, glm::vec3 pointB, glm::vec3 pointC, int m) {
+		glm::vec3 v0, v1, v2;
+		int j;
+		if (m > 0) {
+			for (j = 0; j < 2; j++)
+				v0[j] = (pointA[j] + pointB[j]) * 0.5;
+			for (j = 0; j < 2; j++)
+				v1[j] = (pointA[j] + pointC[j]) * 0.5;
+			for (j = 0; j < 2; j++)
+				v2[j] = (pointB[j] + pointC[j]) * 0.5;
+			divide_triangle(pointA, v0, v1, m - 1);
+			divide_triangle(pointC, v1, v2, m - 1);
+			divide_triangle(pointB, v2, v0, m - 1);
+		}
+		else
+			draw_triangle(pointA, pointB, pointC);
+	};
+	*/
+
+	// Function to draw a single triangle by pushing its vertices
+	auto draw_triangle = [&](const glm::vec3& a, const glm::vec3& b, const glm::vec3& c) {
+		cpuGeom.verts.push_back(a);
+		cpuGeom.verts.push_back(b);
+		cpuGeom.verts.push_back(c);
+
+		// Generate a random color
+		glm::vec3 color(static_cast<float>(rand()) / RAND_MAX,
+			static_cast<float>(rand()) / RAND_MAX,
+			static_cast<float>(rand()) / RAND_MAX);
+
+		// Add the same color for all three vertices of the triangle
+		cpuGeom.cols.push_back(color);
+		cpuGeom.cols.push_back(color);
+		cpuGeom.cols.push_back(color);
+
+		};
+		/*	// Add colors for each vertex
+		cpuGeom.cols.push_back(glm::vec3(1.0f, 0.0f, 0.0f)); // Color for vertex a
+		cpuGeom.cols.push_back(glm::vec3(0.0f, 1.0f, 0.0f)); // Color for vertex b
+		cpuGeom.cols.push_back(glm::vec3(0.0f, 0.0f, 1.0f)); // Color for vertex c
+		};
+		*/
+
+	// Recursive function to subdivide the triangle
+	std::function<void(glm::vec3, glm::vec3, glm::vec3, int)> divide_triangle = [&](glm::vec3 a, glm::vec3 b, glm::vec3 c, int m) {
+		if (m > 0) {
+			// Calculate midpoints of each side
+			glm::vec3 v0 = (a + b) * 0.5f;
+			glm::vec3 v1 = (a + c) * 0.5f;
+			glm::vec3 v2 = (b + c) * 0.5f;
+
+			// Recursively divide the triangles
+			divide_triangle(a, v0, v1, m - 1);
+			divide_triangle(c, v1, v2, m - 1);
+			divide_triangle(b, v2, v0, m - 1);
+		}
+		else {
+			// Draw the triangle when the recursion depth is zero
+			draw_triangle(a, b, c);
+		}
+
+		};
+
+	// Start the recursive subdivision
+	divide_triangle(p1, p2, p3, iterations);
+
+	return cpuGeom;
+}
+
 
 int main() {
 	Log::debug("Starting main");
@@ -180,7 +258,8 @@ int main() {
 	*/
 
 	Parameters p;
-	cpuGeom = generateSin(p);
+	// cpuGeom = generateSin(p);
+	cpuGeom = generateSierpinski(p.iterations);
 	// uploading data to gpu from cpu (note here we're uploading BEFORE THE LOOP, because in this example we're just redrawing the same traingle over and over again)
 	gpuGeom.setVerts(cpuGeom.verts);
 	gpuGeom.setCols(cpuGeom.cols);
@@ -193,9 +272,19 @@ int main() {
 		glfwPollEvents();
 
 		Parameters newP = callbacks->getParameters();
+
+		/*
 		if (newP.isDifferent(p)) { // if difference detected, then we will reupload things
 			p = newP;
 			cpuGeom = generateSin(p);
+			gpuGeom.setVerts(cpuGeom.verts);
+			gpuGeom.setCols(cpuGeom.cols);
+		}
+		*/
+
+		if (newP.iterations != p.iterations) {
+			p = newP;
+			cpuGeom = generateSierpinski(p.iterations);
 			gpuGeom.setVerts(cpuGeom.verts);
 			gpuGeom.setCols(cpuGeom.cols);
 		}
@@ -205,7 +294,7 @@ int main() {
 
 		glEnable(GL_FRAMEBUFFER_SRGB); // tells us to use the srgb color space, flag used (OpenGL is a global state machine)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clearing the current buffers (2, have to clear both). Two buffers, each split into 3 parts, R, G, B, R, G, B
-		glDrawArrays(GL_LINE_STRIP, 0, GLsizei(cpuGeom.verts.size())); // drawing on the buffer
+		glDrawArrays(GL_TRIANGLES, 0, GLsizei(cpuGeom.verts.size())); // drawing on the buffer
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 
 		window.swapBuffers(); // we're using a dual buffer system, so once we're done drawing on one buffer we swap it
